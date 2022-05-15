7b19a3a2901035e1955f9fff57d0906c81bde8ae
youssefkhaleed <68405719+youssefkhaleed@users.noreply.github.com>
2022-05-15 23:51:13 +0200

    youssef updated delays driver

--- delays.c
+++ delays.c
@@ -1,6 +1,6 @@
 #include "tm4c123gh6pm.h"
-
-
+#include "switches_inputs.h"
+#include "switches_inputs.h"
 //extra delay functions
 void delay_milli(int n){
 int i,j;
@@ -19,7 +19,7 @@ for(j=0;j<3;j++)
 //systick timer delays functions : 
 
 void SysTick_Wait(unsigned long delay)  
-{
+{ 
 NVIC_ST_CTRL_R = 0X00;
 NVIC_ST_RELOAD_R = delay-1;
 NVIC_ST_CURRENT_R = 0;
@@ -31,7 +31,12 @@ void SysTick_Wait1ms()
 {
 SysTick_Wait(16000);
 }
-void delay_ms(int num) 
+void SysTick_Wait50ms()
+{ 
+  SysTick_Wait(800000);
+}
+
+	void delay_ms(int num) 
 {
 int i; 
 for(i=0; i<num; i++)
@@ -39,3 +44,21 @@ for(i=0; i<num; i++)
 SysTick_Wait1ms();
 }
 }
+
+int  delay_mil() 
+{
+int i,h; 
+
+	for(i=0; i<20; i++)
+{ if(Switch1_input()==0)
+	{
+	
+		h=pause(); 
+	   if(h==1) return 1;  
+	
+	}
+	
+	SysTick_Wait50ms();
+}
+}
+
