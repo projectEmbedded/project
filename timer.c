91aca1247c1b018fd742881c91e03157003d7750
youssefkhaleed <68405719+youssefkhaleed@users.noreply.github.com>
2022-05-16 15:31:03 +0200

    youssef updated header files of timer driver

--- timer.c
+++ timer.c
@@ -1,8 +1,11 @@
 #include "lcd_drivers.h"
-#include"delays.h"
-#include"leds_on.h"
+#include "delays.h"
+#include "leds_on.h"
+#include "leds_off.h"
+#include "switches_inputs.h"
 void timer(int m,int s)
-{ int y;
+{
+	int y;
 int j=m;
 int i=s;
 int k;
@@ -23,14 +26,15 @@ int k;
 	  LCD4bits_Data((i/10) +'0');       // tens of second
 	  LCD4bits_Data((i%10)+'0');       // units of second 
     k=delay_mil();                   // wait for 1 second
-     if(k==1) 
-	{	
-         leds_off();
-	    return;
-	}                 // check if switch 1 is pressed or not 
-	 LCD4bits_Cmd(0x01);             // clear lcd display 
+	  if(k==1) 
+		{	
+		   leds_off();
+			return;
+		}                 // check if switch 1 is pressed or not 
+		LCD4bits_Cmd(0x01);             // clear lcd display 
 	 }
 i=59;                                // reset second every loop
 }
    finished();
+
 }
