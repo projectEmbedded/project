049cb5651ccd16daf524bd4e462a62d4de3f9e84
habibaibrahim26 <105403205+habibaibrahim26@users.noreply.github.com>
2022-05-17 09:35:11 -0700

    Habiba updated case D in main.c

--- main.c
+++ main.c
@@ -92,72 +92,72 @@ switches();
   {  
 	
 	LCD_WriteString("Cooking Time?");
-  delay_ms(2000);
+        delay_ms(2000);
 	LCD4bits_Cmd(0X01);
 	user_input :
 	LCD_WriteString("maximum 30 min");
+	LCD4bits_Cmd(0xc0);
+	LCD_WriteString("minimum 1 second");
 	delay_ms(2000);
 	LCD4bits_Cmd(0x01);
 	for(h=0 ; h<4 ; h++)
 {
-    LCD4bits_Data(' ');      // printing 4 spaces
+    LCD4bits_Data(' ');          // printing 4 spaces to center the count time
 }
-LCD_WriteString("00:00");    // writing (00:00) on lcd   00:01 >> 30:00 
-temp0=get_keypad_input();        // getting  second digit in min
-buzzer();
-if(temp0=='y')
+LCD_WriteString("00:00");       // writing (00:00) on lcd  
+temp0=get_keypad_input();       // getting  second digit in min (tens)
+buzzer();                       //the buzzer produces an audible tone while pressing on the keypad
+if(temp0=='y')                  // checking if switch1 is pressed
 {
-  LCD4bits_Cmd(0x01);
- goto start;
+  LCD4bits_Cmd(0x01);         //  if pressed then clear the lcd 
+ goto start;                  // then goto idle and wait for a new input
 }
-delay_ms(200); 
+delay_ms(200);               // wait for 200 milliseconds
 cursor_decrement(1);         // shifting cursor to the left
-LCD4bits_Data(temp0);            //writing second digit in min  (00:0k)
-temp1=get_keypad_input();    // getting second digit in min
-  buzzer();     
-  if(temp1=='y')
+LCD4bits_Data(temp0);        //displaying second digit in min  
+temp1=get_keypad_input();    // getting  first digit in min (units)
+  buzzer();                  //the buzzer produces an audible tone while pressing on the keypad
+  if(temp1=='y')              // checking if switch1 is pressed
+ {          
+  LCD4bits_Cmd(0x01);        // if pressed then clear the lcd 
+  goto start;                 // then goto idle and wait for a new input
+ }
+delay_ms(200);                // wait for 200 milliseconds
+cursor_decrement(2);          // shifting the cursor 2 spaces to the left
+LCD4bits_Data(temp0);         // displaying second digit in min
+LCD4bits_Data(temp1);         // displaying first digit in min
+temp2=get_keypad_input();     // getting second digit in second (tens)
+buzzer();                     //the buzzer produces an audible tone while pressing on the keypad
+if(temp2=='y')                // checking if switch1 is pressed
 {
-  LCD4bits_Cmd(0x01);
- goto start;
-}
-delay_ms(200);               
-cursor_decrement(2);
-LCD4bits_Data(temp0);
-LCD4bits_Data(temp1);
-temp2=get_keypad_input();
-buzzer(); 
-if(temp2=='y')
+  LCD4bits_Cmd(0x01);         // if pressed then clear the lcd 
+ goto start;                   // then goto idle and wait for a new input
+} 
+delay_ms(200);                // wait for 200 milliseconds
+cursor_decrement(4);           // shifting the cursor 4 spaces to the left
+LCD4bits_Data(temp0);        // displaying second digit in min
+LCD4bits_Cmd(0x14);          // shifting cursor to the right
+LCD4bits_Data(temp1);        // displaying first digit in min
+LCD4bits_Data(temp2);         // displaying second digit in second
+temp3=get_keypad_input();      // getting first digit in second (units)
+buzzer();                       //the buzzer produces an audible tone while pressing on the keypad 
+if(temp3=='y')                   // checking if switch1 is pressed
 {
-  LCD4bits_Cmd(0x01);
- goto start;
+  LCD4bits_Cmd(0x01);            // if pressed then clear the lcd 
+ goto start;                     // then goto idle and wait for a new input
 }
-delay_ms(200);
-cursor_decrement(4);
-LCD4bits_Data(temp0);
-LCD4bits_Cmd(0x14);
-LCD4bits_Data(temp1);
-LCD4bits_Data(temp2);
-temp3=get_keypad_input();
-buzzer();  
-if(temp3=='y')
-{
-  LCD4bits_Cmd(0x01);
- goto start;
-}
-delay_ms(200);
-cursor_decrement(5);
-LCD4bits_Data(temp0);
-LCD4bits_Data(temp1);
-LCD4bits_Cmd(0x14);
-LCD4bits_Data(temp2);
-LCD4bits_Data(temp3);
-LCD4bits_Cmd(0x01);
-if((temp0==(3+'0')) && (temp1>(0+'0')))
-{
-   goto user_input;
-}
-while(!(GPIO_PORTF_DATA_R&0x01)==0);
-timer((temp0-'0')*10 +(temp1-'0'),(temp2-'0')*10+(temp3-'0'));
+delay_ms(200);                    // wait for 200 milliseconds
+cursor_decrement(5);               // shifting the cursor 5 spaces to the left
+LCD4bits_Data(temp0);             // displaying second digit in min
+LCD4bits_Data(temp1);             // displaying first digit in min
+LCD4bits_Cmd(0x14);                // shifting cursor to the right
+LCD4bits_Data(temp2);              // displaying second digit in second 
+LCD4bits_Data(temp3);               // displaying first digit in second
+LCD4bits_Cmd(0x01);                 // clear the lcd
+if((temp0>=(3+'0')) && (temp1>=(0+'0'))&& (temp2>=(0+'0')) && (temp3>=0+'0')) goto user_input; // check if time input is >30 min then goto userinput again
+ if((temp0=(0+'0')) && (temp1=(0+'0'))&& (temp2=(0+'0')) && (temp3=0+'0')) goto user_input; // check if time input is<1 second then goto userinput again
+while(!(GPIO_PORTF_DATA_R&0x01)==0); // wait unit switch2 is pressed
+timer((temp0-'0')*10 +(temp1-'0'),(temp2-'0')*10+(temp3-'0')); 
  goto start;
   break;
   }
