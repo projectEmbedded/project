829133cae4977aeb395f5bc8002483c5ca909997
minaramzy2000 <104934233+minaramzy2000@users.noreply.github.com>
2022-05-17 23:47:57 +0200

    Update finished.c

--- finished.c
+++ finished.c
@@ -4,14 +4,14 @@
 #include "timer.h"
 void finished(void)
 {   int k;
-	  LCD4bits_Cmd(0x01);
+	        LCD4bits_Cmd(0x01);
 		LCD_WriteString("finished");
 		for(k=0;k<6;k++)
 		{
-      GPIO_PORTA_DATA_R =GPIO_PORTA_DATA_R^0x04;  // toggle the buzzer pin
+                        GPIO_PORTA_DATA_R =GPIO_PORTA_DATA_R^0x04;  // toggle the buzzer pin
 			GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R^0x0E; //toggle the leds
-		  delay_ms(500);
+		       delay_ms(500);
 		}
-		  GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R^0x0E;	
-		LCD4bits_Cmd(0x01);
+		GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R^0x0E; 	
+	        LCD4bits_Cmd(0x01);
 }
