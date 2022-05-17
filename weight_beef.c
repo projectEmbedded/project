12c380e38c8579e83a95a1c5468ef1018db2f148
AhmedTariqq <81717973+Ahmed69582@users.noreply.github.com>
2022-05-17 20:59:29 +0200

    Update weight_beef.c

--- weight_beef.c
+++ weight_beef.c
@@ -9,41 +9,41 @@ void weight_beef(void)
 	
 	 
   int time,j;
-  char n=get_keypad_input(); 
-   buzzer();
+  char n=get_keypad_input();               \\ WE GET THE KEYPAD INPUT TO KNOW THE NUMBER OF KILOS
+   buzzer();				   \\ we make the function buzer to make sound for every time we press on the keypad 
 	  
-	 while (!(n<=(9+'0') && n>=(1+'0')))              
+	 while (!(n<=(9+'0') && n>=(1+'0')))   \\ this loop to make sure that the number we get from the keypad is between 1 and 9            
 			{ 
-			 for(j=0;j<4;j++)
+			 for(j=0;j<4;j++)      \\ the for loop to print space on the lcd and make any thing in the center 
 				{ 
-					LCD4bits_Data(' ');
+					LCD4bits_Data(' '); 
 				
 				}
-			 LCD_WriteString("Err");
-			 delay_ms(2000);
-		   LCD4bits_Cmd(0x01);
-			 LCD_WriteString("Beef weight?");
-		   delay_ms(2000);	                 
-		   LCD4bits_Cmd(0x01);
-			 n=get_keypad_input(); 
-                         buzzer();
+			 LCD_WriteString("Err");     		\\ if the number of kilos not in between of 1 and 9 the lcd will display "ERR"
+			 delay_ms(2000);			\\ we make delay then show the next command 
+		   LCD4bits_Cmd(0x01);				\\ clear the LCD 
+			 LCD_WriteString("Beef weight?");  	\\ then show th text ("Beef weight?") on the LCD 
+		   delay_ms(2000);	                 	\\ we make delay then show the next command 
+		   LCD4bits_Cmd(0x01);				\\ clear the LCD 
+			 n=get_keypad_input(); 			\\ after clearing the LCD we get another input to make sure it is between 1 and 9 
+                         buzzer();			
 			 delay_ms(2000);
 			}
-		         delay_milli(1000);
-			       LCD4bits_Data(n);
+		         delay_milli(1000);			\\ if the number is valid then we will wait for delay 
+			       LCD4bits_Data(n);		\\ display th number of kilos on the LCD
 			       delay_milli(2000);	
-	           LCD4bits_Cmd(0x01);
+	           LCD4bits_Cmd(0x01);				\\ clear the LCd 
        
-			 time=((n-'0'))*30;    
-			 if(time>60)
+			 time=((n-'0'))*30;    			\\ calculate the time after get the number of kilos 
+			 if(time>60)			\\ make sure that the number if larger thean 60 sec then it will be converted to minutes and seconds 
 			 {
-				while(!(GPIO_PORTF_DATA_R&0x01)==0 && !switch3_input()==0);
-			  timer(time/60,(time%60));
+				while(!(GPIO_PORTF_DATA_R&0x01)==0 && !switch3_input()==0);  \\ the while loop to make sure that sw2 is pressed and sw3 open	
+			  timer(time/60,(time%60)); 			\\ get the minutes and the seconds for the function timer 
 			 }
-			 else
+			 else			\\ if the number is not more than 60 seconds it will be converted to seconds only 
 			 {
-				 while(!(GPIO_PORTF_DATA_R&0x01)==0 && !switch3_input()==0);
-				 timer(0,time); 
+				 while(!(GPIO_PORTF_DATA_R&0x01)==0 && !switch3_input()==0);  \\ the while loop to make sure that sw2 is pressed and sw3 open
+				 timer(0,time); 			\\ the timer input (seconds only)
 			 } 
 			 
  }
